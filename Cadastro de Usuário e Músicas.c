d4d08dec53f633b83904f8618c12bf93910a7fa6
Gabriel Ferraz Duque <83476335+gabrielferrazduque@users.noreply.github.com>
2024-06-08 21:26:52 -0300

    Update Cadastro de Usuário e Músicas.c

--- Cadastro de Usuário e Músicas.c
+++ Cadastro de Usuário e Músicas.c
@@ -20,6 +20,36 @@ struct Playlist {
 #define MAX_USUARIOS 100
 #define MAX_MUSICAS 1000
 
+void listarMusicas(struct Musica musicas[], int qtdMusicas) {
+    for (int i = 0; i < qtdMusicas; i++) {
+        printf("Codigo: %d, Titulo: %s, Artista: %s\n", musicas[i].codigo, musicas[i].titulo, musicas[i].artista);
+    }
+}
+
+void consultarMusica(struct Musica musicas[], int qtdMusicas, char *query) {
+    for (int i = 0; i < qtdMusicas; i++) {
+        if (strstr(musicas[i].titulo, query) != NULL || strstr(musicas[i].artista, query) != NULL || musicas[i].codigo == atoi(query)) {
+            printf("Codigo: %d, Titulo: %s, Artista: %s\n", musicas[i].codigo, musicas[i].titulo, musicas[i].artista);
+        }
+    }
+}
+Funções de Gerenciamento de Playlists
+
+c
+Copiar código
+void listarPlaylists(struct Playlist playlists[], int qtdPlaylists, struct Usuario usuarios[], int qtdUsuarios) {
+    for (int i = 0; i < qtdPlaylists; i++) {
+        for (int j = 0; j < qtdUsuarios; j++) {
+            if (playlists[i].codUsuario == usuarios[j].codigo) {
+                printf("Codigo: %d, Titulo: %s, Criador: %s\n", playlists[i].codigo, playlists[i].titulo, usuarios[j].nome);
+                for (int k = 0; k < playlists[i].qtdMusicas; k++) {
+                    printf("   Musica %d\n", playlists[i].musicas[k]);
+                }
+            }
+        }
+    }
+}
+
 Usuario usuarios[MAX_USUARIOS];
 Musica musicas[MAX_MUSICAS];
 int totalUsuarios = 0;
